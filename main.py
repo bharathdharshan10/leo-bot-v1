6d3e66b4ed366479ce4fb9399cc7d62f4af48d5a
bharathdharshan10 <bharathdharshan10@gmail.com>
2026-05-19 11:40:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 #PACKAGES
 #--------------------
 
-
-from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from groq import Groq
 from pydantic import BaseModel
 import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+app.add_middleware(
+        CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/chat")
+def chat(data:dict):
+    return {"reply":data["message"]}
 #------------------------
 #SERVER CSS AND JS FILE
 #------------------------
@@ -133,6 +144,7 @@ async def home():
     return FileResponse("static/index.html")
 
 
+
 app.mount("/static",
 StaticFiles(directory="static"),
 name="static")
@@ -172,3 +184,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
 
     uvicorn.run(app, host="0.0.0.0", port=port)
+ 
